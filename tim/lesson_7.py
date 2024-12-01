9fcfb876d2a6672b9f105bd701d4c85641f6c2a2
archer73 <mikhail.povor.77@mail.ru>
2024-12-01 12:02:59 +0300

    first commit

---
+++ tim/lesson_7.py
@@ -0,0 +1,41 @@
+# A = [0] * 10000
+# n = 0
+#
+# x = int(input())
+# A[n] = x
+# n+=1
+
+# A = [1,2,3,4,]
+# x = int(input())
+# A.append(x) # метод
+# n = len(A)
+# print(A)
+#
+# b= A.pop(x) # удаляет с конца]
+
+#  List comprehention
+
+# A = [x**2  for x in range(10)]
+# # эквивалентно
+# A = []
+# for x in range (10):
+#     A.append(x**2)
+
+
+# A = [1,2,3,4,5,7,12,9,6]
+# b = []
+# for x in A:
+#     if x%2==0:
+#         b.append(x)
+#
+# print(b)
+
+# Эквивалентно\
+#
+# A= [1,2,3,4,5,7,12,9,6]
+# B = [x**2 for x in A if x%2==0]
+#
+#
+
+
+# Квадратичные сортировки
