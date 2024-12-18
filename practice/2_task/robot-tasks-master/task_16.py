17dba79e4838ba4a3b6e3e248ec842e0b9271acb
archer73 <mikhail.povor.77@mail.ru>
2024-12-18 21:15:28 +0300

    com_2

--- practice/2_task/robot-tasks-master/task_16.py
+++ practice/2_task/robot-tasks-master/task_16.py
@@ -5,7 +5,15 @@ from pyrob.api import *
 
 @task
 def task_8_22():
-    pass
+    while not wall_is_above():
+        move_up()
+        if wall_is_on_the_left() and wall_is_above():
+            while not wall_is_on_the_right():
+                move_right()
+        elif wall_is_on_the_right() and wall_is_above():
+            while not wall_is_on_the_left():
+                move_left()
+
 
 
 if __name__ == '__main__':
--- practice/2_task/robot-tasks-master/task_17.py
+++ practice/2_task/robot-tasks-master/task_17.py
@@ -5,7 +5,12 @@ from pyrob.api import *
 
 @task
 def task_8_27():
-    pass
+    while not cell_is_filled():
+        move_up()
+    if cell_is_filled():
+        move_left()
+    if not  cell_is_filled():
+        move_right(2)
 
 
 if __name__ == '__main__':
