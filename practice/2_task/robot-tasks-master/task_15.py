3497ee1600913e2da163eeb2280e3cdf59cda63d
archer73 <mikhail.povor.77@mail.ru>
2024-12-18 20:23:44 +0300

    18.12.2024

--- practice/2_task/robot-tasks-master/task_15.py
+++ practice/2_task/robot-tasks-master/task_15.py
@@ -5,7 +5,29 @@ from pyrob.api import *
 
 @task
 def task_8_21():
-    pass
+    if wall_is_on_the_right() and wall_is_above():
+        for i in range(9):
+            move_left(1)
+            move_down(1)
+    elif wall_is_above() and wall_is_on_the_left():
+        for i in range(9):
+            move_right(1)
+            move_down(1)
+    elif wall_is_on_the_left() and wall_is_beneath():
+        for i in range(9):
+            move_right(1)
+            move_up(1)
+
+    elif wall_is_beneath() and wall_is_on_the_left():
+        for i in range(9):
+            move_left(1)
+            move_up(1)
+
+    elif wall_is_beneath() and wall_is_on_the_right():
+        for i in range(9):
+            move_left(1)
+            move_up(1)
+
 
 
 if __name__ == '__main__':
