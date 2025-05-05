1556ab66fa651487829e6234882787ea3974897c
archer73 <mikail.povor.77@mail.ru>
2025-05-05 22:58:40 +0300

    lesson_4

---
+++ tim/lesson_4/1_hello.py
@@ -0,0 +1,24 @@
+#def hello(name = "World"):
+#    print("Hello," , name)
+#hello(")
+#f = hello
+#f()
+ 
+
+#def max2(x, y):
+#    if x > y:
+#        return x
+
+#    return y
+
+#print(max2(5,6))
+
+#def max3(x,y,z):
+#    return max2(x,(max2(y,z))
+#print(max3(5,2,7)
+
+def hello_separated(separator="-",name="World"):
+    print("Hello",name,sep=separator)
+hello_separated(name = "John",
+      separator = "***")
+ 
