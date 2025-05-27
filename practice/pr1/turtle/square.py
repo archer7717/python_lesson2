3c3fb92a6f89fd1ca6a5c6ba5a2c5768f8d14bf5
archer7717 <mikhail.povor.77@mail.ru>
2025-05-27 16:15:43 +0300

    pr_5_done

--- practice/pr1/turtle/square.py
+++ practice/pr1/turtle/square.py
@@ -1,24 +1,8 @@
-import turtle
+import turtle as t
+t.shape('turtle')
+s = 10
+for i in range(100):
+    t.forward(s)
+    t.left(90)
+    s+=5
 
-turtle.shape('turtle')
-turtle.forward(30)
-turtle.left(90)
-turtle.forward(30)
-turtle.left(90)
-turtle.forward(30)
-turtle.left(90)
-turtle.forward(30)
-turtle.penup()
-turtle.forward(10)
-turtle.left(90)
-
-turtle.backward(10)
-turtle.pendown()
-
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
-turtle.left(90)
-turtle.forward(50)
