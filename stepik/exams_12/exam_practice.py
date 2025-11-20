a7f08bad0c176836d1687914b8bf065f4b1a0c4b
Michael Povoroznuk <mikhail.povor.77@mail.ru>
2025-11-20 14:13:48 +0300

    test_12_done

---
+++ stepik/exams_12/exam_practice.py
@@ -0,0 +1,25 @@
+#FIXME
+# numbers = [1, 2, 3, 4, 5]
+# my_list = numbers[:-1]
+# print(my_list)
+
+#FIXME
+# a = [int(i) for i in input().split()]
+# b = 0
+# for i in range(len(a)):
+#     b = b + a[i]
+# print(*a,sep='+',end='=')
+# print(b)
+
+#FIXME
+# n = [i for i in input().split('-')]
+# s = [len(q) for q in n]
+# if ''.join(n).isdigit() and s == [3, 3, 4]:
+#     print('YES')
+# elif ''.join(n).isdigit() and s == [1, 3, 3 , 4] and n[0] == '7':
+#     print('YES')
+# else:
+#     print('NO')
+
+
+
