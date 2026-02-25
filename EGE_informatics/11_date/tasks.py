475e17136e5808dd1af37fdf9ab483e498e0eecb
Michael Povoroznuk <mikhail.povor.77@mail.ru>
2026-02-25 22:07:35 +0300

    done_for_this_dat

--- EGE_informatics/11_date/tasks.py
+++ EGE_informatics/11_date/tasks.py
@@ -24,19 +24,46 @@
 #         print(k)
 
 
+# from math import *
+# for l in range(2, 1000):
+#     kod = 52 + 10 + 458
+#     char = ceil(log2(kod))
+#     number = ceil(l * char/8)
+#     if number*862 <= 276*2**10:
+#         print(l)
 
 
 
+from math import *
+
+# for l in range(2 ,1000):
+#     kod = 26+10+450
+#     word = ceil(log2(kod))
+#     number = ceil(word * l/8)
+#     if number*708 >= 213*2**10:
+#         print(l)
+
+# for l in range(2, 1000):
+#     char = ceil(log2(l))
+#     number = ceil(char * 80/8)
+#     if number*1200 <= 150*2**10:
+#         print(l)
 
 
+# for dop in range(1, 100):
+#     kod = 510 + 10
+#     numb = ceil(log2(kod))
+#     word = ceil(numb*99/8)
+#     if 4322*(word+dop) > 543*2**10:
+#         print(dop)
+#         break
 
-
-
-
-
-
-
-
+for dop in range(1, 100):
+    kod = 10 + 999
+    numb = ceil(log2(kod))
+    word = ceil(numb*745/8)
+    if 312*(word+dop) <= 311*2**10:
+        print(312*dop)
 
 
 
